be2d4fb14433a4acaa515577693218fb1f3c2848
AarifLamat <aarif.lamat@outlook.com>
2024-05-16 20:25:00 +0800

    20 server start script

---
+++ start20stations.sh
@@ -0,0 +1,20 @@
+./station-server TerminalA 4001 4002 localhost:4012 localhost:4016 localhost:4024 localhost:4026 localhost:4038 &
+./station-server JunctionB 4003 4004 localhost:4014 localhost:4024 localhost:4032 localhost:4038 &
+./station-server BusportC 4005 4006 localhost:4016 localhost:4020 localhost:4030 localhost:4032 localhost:4038 &
+./station-server StationD 4007 4008 localhost:4012 localhost:4018 &
+./station-server TerminalE 4009 4010 localhost:4022 localhost:4024 &
+./station-server JunctionF 4011 4012 localhost:4002 localhost:4008 localhost:4032 &
+./station-server BusportG 4013 4014 localhost:4004 localhost:4022 localhost:4024 localhost:4038 &
+./station-server StationH 4015 4016 localhost:4002 localhost:4006 localhost:4020 &
+./station-server TerminalI 4017 4018 localhost:4008 localhost:4026 &
+./station-server JunctionJ 4019 4020 localhost:4006 localhost:4016 localhost:4036 localhost:4038 localhost:4040 &
+./station-server BusportK 4021 4022 localhost:4010 localhost:4014 localhost:4024 localhost:4030 localhost:4032 &
+./station-server StationL 4023 4024 localhost:4002 localhost:4004 localhost:4010 localhost:4014 localhost:4022 localhost:4026 localhost:4030 &
+./station-server TerminalM 4025 4026 localhost:4002 localhost:4018 localhost:4024 localhost:4028 localhost:4032 localhost:4036 &
+./station-server JunctionN 4027 4028 localhost:4026 localhost:4036 &
+./station-server BusportO 4029 4030 localhost:4006 localhost:4022 localhost:4024 localhost:4032 &
+./station-server StationP 4031 4032 localhost:4004 localhost:4006 localhost:4012 localhost:4022 localhost:4026 localhost:4030 localhost:4038 &
+./station-server TerminalQ 4033 4034 localhost:4038 &
+./station-server JunctionR 4035 4036 localhost:4020 localhost:4026 localhost:4028 &
+./station-server BusportS 4037 4038 localhost:4002 localhost:4004 localhost:4006 localhost:4014 localhost:4020 localhost:4032 localhost:4034 &
+./station-server StationT 4039 4040 localhost:4020 &
